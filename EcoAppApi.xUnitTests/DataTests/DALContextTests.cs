9615651a63e6d2fc938a129d415e5b5b2e879901
Itamar Herr <itamarherr@gmail.com>
2025-01-10 10:33:04 +0200

    Add unit sest Xunit

---
+++ EcoAppApi.xUnitTests/DataTests/DALContextTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.CodeAnalysis.Editing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+namespace EcoAppApi.xUnitTests.DataTests;
+
+public class DALContextTests
+{
+    [Fact]
+public void testAdd()
+    {
+        Assert.True(true);
+    }
+}
---
+++ EcoAppApi.xUnitTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace EcoAppApi.xUnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
